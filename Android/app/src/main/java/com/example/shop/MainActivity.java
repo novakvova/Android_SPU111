c6dc6a1c9fc5d84f323706494423ecf5ffc971f2
novakvova <novakvova@gmail.com>
2024-04-06 12:03:23 +0300

    Get request server

--- Android/app/src/main/java/com/example/shop/MainActivity.java
+++ Android/app/src/main/java/com/example/shop/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.shop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.shop.application.HomeApplication;
+import com.example.shop.dto.category.CategoryItemDTO;
+import com.example.shop.services.ApplicationNetwork;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,15 +26,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //знаходимо елемент на сторінці
-        ImageView ivAvatar = findViewById(R.id.ivAvatar);
+        //ImageView ivAvatar = findViewById(R.id.ivAvatar);
         //Server ip
         //http://10.0.2.2:5297/images/1.jpg
 //        String url = "https://content1.rozetka.com.ua/goods/images/big/415679366.jpg";
 //        String url = "http://10.0.2.2:5297/images/1.jpg";
-        String url = "https://spu111.itstep.click/images/1.jpg";
-        Glide.with(HomeApplication.getAppContext())
-                .load(url)
-                .apply(new RequestOptions().override(400))
-                .into(ivAvatar);
+//        String url = "https://spu111.itstep.click/images/1.jpg";
+//        Glide.with(HomeApplication.getAppContext())
+//                .load(url)
+//                .apply(new RequestOptions().override(400))
+//                .into(ivAvatar);
+
+        ApplicationNetwork
+                .getInstance()
+                .getCategoriesApi()
+                .list()
+                .enqueue(new Callback<List<CategoryItemDTO>>() {
+                    @Override
+                    public void onResponse(Call<List<CategoryItemDTO>> call, Response<List<CategoryItemDTO>> response) {
+                        if(response.isSuccessful()) {
+                            List<CategoryItemDTO> items = response.body();
+                            //int count = items.size();
+                            //Log.d("---count---", String.valueOf(count));
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<CategoryItemDTO>> call, Throwable throwable) {
+                        Log.e("--problem--", "error server");
+                    }
+                });
     }
 }
