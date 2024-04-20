20b4ca2cbc0bc72995ebdb617b2f452f59f849c3
novakvova <novakvova@gmail.com>
2024-04-20 09:33:01 +0300

    Add CRUD Images

--- WebStore/WebStore/Controllers/CategoriesController.cs
+++ WebStore/WebStore/Controllers/CategoriesController.cs
@@ -45,7 +45,7 @@ namespace WebStore.Controllers
                 {
                     x.Resize(new ResizeOptions
                     {
-                        Size = new Size(1200),
+                        Size = new Size(1920),
                         Mode = ResizeMode.Max
                     });
                 });
@@ -63,12 +63,38 @@ namespace WebStore.Controllers
         }
 
         [HttpPut]
-        public IActionResult Edit([FromBody] CategoryEditViewModel model)
+        public async Task<IActionResult> Edit([FromBody] CategoryEditViewModel model)
         {
             var category = _appContext.Categories.SingleOrDefault(x => x.Id == model.Id);
             if (category == null)
             {
                 return NotFound();
+            }
+            if (model.Image != null)
+            {
+                string? imgDel = category.Image;
+                if (imgDel != null)
+                {
+                    string imgDelPath = Path.Combine(Directory.GetCurrentDirectory(), "images", imgDel);
+                    if (System.IO.File.Exists(imgDelPath))
+                    {
+                        System.IO.File.Delete(imgDelPath);
+                    }
+                }
+                using Image image = Image.Load(model.Image.OpenReadStream());
+                image.Mutate(x =>
+                {
+                    x.Resize(new ResizeOptions
+                    {
+                        Size = new Size(1200),
+                        Mode = ResizeMode.Max
+                    });
+                });
+                string imageName = Path.GetRandomFileName() + ".webp";
+                string dirSaveImage = Path.Combine(Directory.GetCurrentDirectory(), "images", imageName);
+
+                await image.SaveAsync(dirSaveImage, new WebpEncoder());
+                category.Image = imageName;
             }
             category.Description = model.Description;
             category.Name = model.Name;
@@ -83,6 +109,15 @@ namespace WebStore.Controllers
             if (category == null)
             {
                 return NotFound();
+            }
+            string? imgDel = category.Image;
+            if (imgDel != null)
+            {
+                string imgDelPath = Path.Combine(Directory.GetCurrentDirectory(), "images", imgDel);
+                if (System.IO.File.Exists(imgDelPath))
+                {
+                    System.IO.File.Delete(imgDelPath);
+                }
             }
             _appContext.Categories.Remove(category);
             _appContext.SaveChanges();
